59c7671ad56a474a3b3229e7dbc0760baa95eeb1
Nick Inman <nick@theoatmeal.com>
2019-02-19 18:36:41 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  post '/logout' => 'sessions#destroy'
+  get '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
   get '/show' => 'secrets#show'
   # The priority is based upon order of creation: first created -> highest priority.
