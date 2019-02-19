f231d89097772225bb04e9d3cb486b888f42c139
Nick Inman <nick@theoatmeal.com>
2019-02-19 18:45:21 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   def create
     if params[:name].nil? || params[:name] == ''
       redirect_to sessions_new_path
-    else 
+    else
       session[:name] = params[:name]
       redirect_to secrets_show_path
     end
